0794ed90b3920c1cb76eafec38c54a6a74b05a1e
Ruhanyat-994 <alruhanyat994@gmail.com>
2025-05-27 14:58:09 +0600

    added the assertions

---
+++ src/test/java/FirstTestSuit/pages/DashboardPage.java
@@ -0,0 +1,34 @@
+package FirstTestSuit.pages;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class DashboardPage {
+    private final Page page;
+    private Page popupPage;
+
+    public DashboardPage(Page page) {
+        this.page = page;
+    }
+
+    /**
+     * Waits for the popup triggered by clicking a link with given popupTriggerName,
+     * then navigates to a section link with given sectionLinkName in the popup.
+     *
+     * @param popupTriggerName The name of the link that triggers the popup (e.g., "Florida")
+     * @param sectionLinkName  The name of the link to click inside the popup (e.g., "Compliance")
+     */
+    public void openPopupAndNavigate(String popupTriggerName, String sectionLinkName) {
+        popupPage = page.waitForPopup(() -> {
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(popupTriggerName)).click();
+        });
+
+        assertThat(popupPage.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(sectionLinkName))).isVisible();
+        popupPage.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(sectionLinkName)).click();
+    }
+
+    public Page getPopupPage() {
+        return popupPage;
+    }
+}
--- src/test/java/FirstTestSuit/pages/LoginPage.java
+++ src/test/java/FirstTestSuit/pages/LoginPage.java
@@ -1,19 +1,33 @@
 package FirstTestSuit.pages;
 
-
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LoginPage {
     private final Page page;
+    private final String url = "https://manage.demo.goldenstateregistry.org/admin";
 
     public LoginPage(Page page) {
         this.page = page;
     }
 
-    public void login(String username, String password) {
-        page.fill("#username", username);
-        page.fill("#password", password);
+    public void login(String email, String password) {
+        // Navigate and assert URL
+        page.navigate(url);
+        assertThat(page).hasURL(url);
+
+        // Fill email
+        Locator emailField = page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter email..."));
+        assertThat(emailField).isVisible();
+        emailField.fill(email);
+
+        // Fill password
+        Locator passwordField = page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Password"));
+        assertThat(passwordField).isVisible();
+        passwordField.fill(password);
+
+        // Click Sign In
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign In")).click();
     }
 }
